0fc82611f778b32d232cc18f389b9d6d0fcad07f
shubhamarman13 <shubhamarman13@gmail.com>
2024-06-16 01:36:11 +0530

    dynamic array in whichj size is asked by the user at the run time and by using  a function return a given sized array

---
+++ Dynamic_Array_size_is_decided_at_runtime.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int *Array(int size)
+{
+    int *ptr;
+    ptr= new int[size];
+    cout<<"Enter the elements "<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>ptr[i];
+    }
+    cout<<endl<<"****************************"<<endl;
+    return ptr;
+}
+int main()
+{
+int *arr , size;
+cout<<"Enter the size that you want in the dynamic Array"<<endl;
+cin>>size;
+arr=Array(size);
+cout<<endl<<"****************************"<<endl;
+for (int i = 0; i < size; i++)
+{
+    cout<<arr[i]<<endl;
+}
+
+
+}
