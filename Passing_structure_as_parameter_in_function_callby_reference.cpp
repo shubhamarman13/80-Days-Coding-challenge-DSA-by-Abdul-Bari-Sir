045ba44ff27c060c734208418f42d5f9c39cd15a
shubhamarman13 <shubhamarman13@gmail.com>
2024-06-17 16:49:15 +0530

    tested a code  for structure as parameter in the array

--- Passing_structure_as_parameter_in_function_callby_reference.cpp
+++ Passing_structure_as_parameter_in_function_callby_reference.cpp
@@ -1,5 +1,6 @@
+//call by reference in the function of structre 
 // passing entire structure as parameter in the function and in the end it returing the value as claculation of Area as length*breath;
-// it is an example of call  by value in the function    
+// it is an example of call  reference  in the function    
 
 #include<iostream>
 using namespace std;
@@ -9,7 +10,7 @@ struct Rectangle
     int breath;
 
 };
-int Area(struct Rectangle r)   // here we are pssing the structure as parameter and it is call  by value 
+int Area(struct Rectangle &r)   // here we are pssing the structure as parameter and it is call  by value 
 {
     r.lenght=200;   
     return r.breath*r.lenght;
@@ -21,6 +22,7 @@ int main()
     r1.lenght=10;
     r1.lenght=10;
     r1.breath=20;
-    cout<<"Area= "<<Area(r1);
+    cout<<"Area= "<<Area(r1)<<endl;
+    cout<<r1.lenght;ttt
 
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
-
+struct Rectangle 
+{
+int length;
+int breath;
+};
+int Area(struct Rectangle &r2)
+{
+    cout<<"Enter the length"<<endl;
+    cin>>r2.length;
+    cout<<"Enter the breath"<<endl;
+    cin>>r2.breath;
+return r2.breath*r2.length;
+}
 int main()
 {
-    int Arr[]= {2,3,4,5,6,7,8};
-    int n =7;
+ struct Rectangle r;
+ cout<<Area(r)<<endl;
+ cout<<r.length;
 }
