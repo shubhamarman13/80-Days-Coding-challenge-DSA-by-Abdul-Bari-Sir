25cc29ea0f47356b266fa3e8770b356ebdb3afbb
shubhamarman13 <shubhamarman13@gmail.com>
2024-07-04 13:39:06 +0530

    virtual function code with example

---
+++ test2.cpp
@@ -0,0 +1,9 @@
+// #include<iostream>
+// using namespace std;
+int main()
+{
+    int x=20;
+    int y=28;
+    int z=x+y;
+    return true;
+}
--- virtual_function_proper_example.cpp
+++ virtual_function_proper_example.cpp
@@ -37,6 +37,7 @@ int main()
 {
    
    sound *obj;
+   
    dog obj2;
    obj=&obj2;
    obj->speak();
