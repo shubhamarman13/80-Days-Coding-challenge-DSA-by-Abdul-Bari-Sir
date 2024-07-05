52c1585f4b7917af4d283d5d193c712aa2e165d6
shubhamarman13 <shubhamarman13@gmail.com>
2024-07-05 22:48:51 +0530

    day 12 code  updated

--- use_Of_virtual_function.cpp
+++ use_Of_virtual_function.cpp
@@ -25,16 +25,30 @@ class square : public  shape
 
     }
 };
+class rectangle : public shape
+{
+    public:
+    void draw() override
+    {
+        cout<<"Drawing the rectangle "<<endl;
+    }
+};
 int main()
 {
     shape* obj;
     shape* obj2;
+    shape* obj3;
     circle cir;
     square sqr;
+    rectangle rec;
+    
     obj= &cir;
     obj2= &sqr;
+    obj3 = &rec;
     obj->shape::draw();  // in this line we are calling the method of the base class as draw() in the main function directly ;
     obj->draw();
     obj2->draw();    
+    obj3->draw();
+    
 
 }
